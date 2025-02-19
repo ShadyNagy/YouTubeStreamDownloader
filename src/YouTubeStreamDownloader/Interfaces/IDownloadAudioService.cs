7a2495e2fec2e3d0c5cd4f05e6eea8b928d3c3e8
ShadyNagy <info@shadynagy.com>
2025-02-19 11:48:41 +0200

    v1.1.9

--- src/YouTubeStreamDownloader/Interfaces/IDownloadAudioService.cs
+++ src/YouTubeStreamDownloader/Interfaces/IDownloadAudioService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 namespace YouTubeStreamDownloader.Interfaces;
 public interface IDownloadAudioService
 {
+  Task<string> DownloadAudioWithProgressAsync(string videoUrl, IProgress<double>? progress, CancellationToken cancellationToken = default);
   Task<string> DownloadAudioWithProgressAsync(string videoUrl, string outputPath, IProgress<double>? progress, CancellationToken cancellationToken = default);
   Task<string> DownloadAudioAsFileAsync(string videoUrl, string outputPath, IProgress<double>? progress, CancellationToken cancellationToken = default);
   Task<string> DownloadAudioAsFileAsync(string videoUrl, string outputPath, CancellationToken cancellationToken = default);
